542ee8593ed000be54a72aa1608e97ffdb7636fe
DemetriSam <samartsew@gmail.com>
2022-09-22 11:15:11 +0300

    move functions

--- src/Cli.php
+++ src/Cli.php
@@ -9,31 +9,3 @@ function greeting()
 {
     line('Welcome to the Brain Games!');
 }
-
-function askName()
-{
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
-
-    return $name;
-}
-
-function checkAnswer($rightAnswer, $answer, $name)
-{
-    if ($answer == $rightAnswer) {
-        line('Correct!');
-        return true;
-    } else {
-        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $rightAnswer);
-        line('Let\'s try again, %s!', $name);
-        return false;
-    }
-}
-
-function askQuestion($question)
-{
-    line("Question: %s", $question);
-    $answer = prompt('Your answer');
-
-    return $answer;
-}
--- src/Functions.php
+++ src/Functions.php
@@ -5,6 +5,34 @@ namespace Brain\Cli;
 use function cli\line;
 use function cli\prompt;
 
+function askName()
+{
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+    return $name;
+}
+
+function checkAnswer($rightAnswer, $answer, $name)
+{
+    if ($answer == $rightAnswer) {
+        line('Correct!');
+        return true;
+    } else {
+        line("'%s' is wrong answer ;(. Correct answer was '%s'.", $answer, $rightAnswer);
+        line('Let\'s try again, %s!', $name);
+        return false;
+    }
+}
+
+function askQuestion($question)
+{
+    line("Question: %s", $question);
+    $answer = prompt('Your answer');
+
+    return $answer;
+}
+
 function gcd($a, $b)
 {
     $r = $a % $b;
