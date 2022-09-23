aca485e983644058e4abe32b4ddfe0ac9555152e
DemetriSam <samartsew@gmail.com>
2022-09-23 14:06:31 +0300

    refactor brain-progression

--- src/Games/progression.php
+++ src/Games/progression.php
@@ -4,34 +4,33 @@ namespace Brain\Cli;
 
 use function Brain\Cli\isPrime;
 
+const PGS_RULES = 'What number is missing in the progression?';
+const PGS_START_RANGE = [0, 10];
+const PGS_STEP_RANGE = [0, 20];
+const PGS_LENGTH_OF_SEQUENCE = 10;
+
 function progressionGame()
 {
-    $gameSet = [
-        'rules' => 'What number is missing in the progression?',
-        'set' => [],
-    ];
+    $rounds = [];
 
-    $questionsCount = 3;
-    $startRange = [0, 10];
-    $stepRange = [0, 20];
-    $lengthOfSequence = 10;
-    $sequence = [];
+    for ($i = 0; $i < ROUNDS_COUNT; $i++) {
 
-    for ($i = 0; $i < $questionsCount; $i++) {
-        $start = rand(...$startRange);
-        $step = rand(...$startRange);
+        $sequence = [];
 
-        for ($x = 0; $x < $lengthOfSequence; $x++) {
+        $start = rand(...PGS_START_RANGE);
+        $step = rand(...PGS_STEP_RANGE);
+
+        for ($x = 0; $x < PGS_LENGTH_OF_SEQUENCE; $x++) {
             $sequence[$x] = $start + $x * $step;
         }
 
-        $missingValueIndex = rand(0, $lengthOfSequence - 1);
+        $missingValueIndex = rand(0, PGS_LENGTH_OF_SEQUENCE - 1);
         $rightAnswer = $sequence[$missingValueIndex];
         $sequence[$missingValueIndex] = '..';
         $question = implode(' ', $sequence);
 
-        $gameSet['set'][] = [$question, $rightAnswer];
+        $rounds[] = [$question, $rightAnswer];
     }
 
-    return $gameSet;
+    launch(PGS_RULES, $rounds);
 }
